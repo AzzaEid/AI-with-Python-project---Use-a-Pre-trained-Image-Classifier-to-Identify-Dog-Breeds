aa016df201a0c9df07b7e9158ab324bf85a7ae2b
AZZA <azzaabed2003@gmail.com>
2024-06-02 09:46:49 +0300

    second submission

--- get_input_args.py
+++ get_input_args.py
@@ -43,8 +43,8 @@ def get_input_args():
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
     parser.add_argument('--dir',type=str,default='pet_images/',
                         help='path to folder of images')
-    parser.add_argument('arch',default='vgg')
-    parser.add_argument('--dogfile', default='dogname.txt')
+    parser.add_argument('--arch',default='vgg',help="CNN Model Architecture")
+    parser.add_argument('--dogfile', default='dognames.txt',help="folder with a list of valid dognames")
     
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
